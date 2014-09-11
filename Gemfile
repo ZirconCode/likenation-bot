dd921abd1913d22b5aac876500d98f4d3af2666a
ZirconCode <gruenings@gmail.com>
2014-09-11 13:00:10 +0900

    initial cleanup, ff path, logging, profile attempt

--- Gemfile
+++ Gemfile
@@ -3,5 +3,5 @@ source "https://rubygems.org"
 
 # gem "rails"
 
-gem 'selenium-webdriver'
+gem 'selenium-webdriver', '2.41.0' # version correlates to Firefox 28 (see http://selenium.googlecode.com/git/rb/CHANGES)
 gem 'headless' # requires xvfb 'sudo apt-get install xvfb'
--- config.rb
+++ config.rb
@@ -1,6 +1,6 @@
 module Configuration  
-	NAME  = 'likenation account name'
-	PASS  = 'likenation account password'
-	HLESS = true # run headerless?
+	NAME	= 'name@name.name'
+	PASS	= 'spacebanana'
+	HLESS	= false # run headerless?
+	FFPATH	= 'firefox/firefox' # path to firefox to use, leave empty to use default
 end
-
